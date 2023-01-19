5020e3dc886877061f2ec82a366c665b13c5c2ed
Chouteau <tetyana.chouteau@passculture.app>
2023-01-19 10:09:30 +0100

    first commit

---
+++ index.tsx
@@ -0,0 +1,12 @@
+const n: number = parseInt(readline());
+for (let i = 0; i < n; i++) {
+  var inputs: string[] = readline().split(" ");
+  const b: string = inputs[0];
+  const c: number = parseInt(inputs[1]);
+}
+const s: string = readline();
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log("Hello World !");
