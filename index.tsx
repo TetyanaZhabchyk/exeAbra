2ed0707cac285e54462e0f0aad5f2e8b381caf82
Chouteau <tetyana.chouteau@passculture.app>
2023-01-23 13:31:47 +0100

    Création de table

--- index.tsx
+++ index.tsx
@@ -1,24 +1,14 @@
-//il faut comprendre l'énoncé.
-
-//1) il faut afficher le code (dans le boucle avec le console.error fait afficher c et b) 
-//2) faut comprendre que c c'est le code ASCII d'une lettre on peut obtenir la lettre avec la méthode String.fromCharCode
-//3) après j'avoue que je sais pas trop comment n'y prendre car
-// le code n'a pas tjs la même taille 
-//(dans l'exemple abracadabra 1 vaut a mais 001 vaut b. 
-//Il y a donc un certain nombre de combinaisons à tester 
-//avant de trouver celle qui fonctionne. Dans cet exemple, 
-//on aurait pu commencer par 100 au lieu de dire que 1 -> a,
-// mais on voit tout de suite que 100 n'existe pas.
-
-const n: number = parseInt(readline());
+const n = parseInt(readline());
+let coder = [];
 for (let i = 0; i < n; i++) {
-  var inputs: string[] = readline().split(" ");
-  const b: string = inputs[0];
-  const c: number = parseInt(inputs[1]);
+  var inputs = readline().split(" ");
+  const b = inputs[0];
+  const c = parseInt(inputs[1]);
+
+  //console.error({b, c}
+  console.error(String.fromCharCode(c) + " -> " + b);
+  // on créer une table qui va permettre à chaque lettre
+  //d'avoir son code
+  coder[String.fromCharCode(c)] = b;
 }
-const s: string = readline();
-
-// Write an answer using console.log()
-// To debug: console.error('Debug messages...');
-
-console.log("Hello World... ...................!"[Symbol];
+console.error(coder);
