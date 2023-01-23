d5e393868c751a531375c6c8053dfe6b36f2f576
Chouteau <tetyana.chouteau@passculture.app>
2023-01-23 10:23:41 +0100

    todo

--- index.tsx
+++ index.tsx
@@ -1,3 +1,15 @@
+//il faut comprendre l'énoncé.
+
+//1) il faut afficher le code (dans le boucle avec le console.error fait afficher c et b) 
+//2) faut comprendre que c c'est le code ASCII d'une lettre on peut obtenir la lettre avec la méthode String.fromCharCode
+//3) après j'avoue que je sais pas trop comment n'y prendre car
+// le code n'a pas tjs la même taille 
+//(dans l'exemple abracadabra 1 vaut a mais 001 vaut b. 
+//Il y a donc un certain nombre de combinaisons à tester 
+//avant de trouver celle qui fonctionne. Dans cet exemple, 
+//on aurait pu commencer par 100 au lieu de dire que 1 -> a,
+// mais on voit tout de suite que 100 n'existe pas.
+
 const n: number = parseInt(readline());
 for (let i = 0; i < n; i++) {
   var inputs: string[] = readline().split(" ");
