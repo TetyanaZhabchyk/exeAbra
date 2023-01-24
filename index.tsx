b221bf5697a84df15130833852c0509b69c85cb4
Chouteau <tetyana.chouteau@passculture.app>
2023-01-24 13:21:12 +0100

    coder

--- index.tsx
+++ index.tsx
@@ -1,14 +1,15 @@
 const n = parseInt(readline());
-let coder = [];
+
+let coder = {};
+
 for (let i = 0; i < n; i++) {
   var inputs = readline().split(" ");
   const b = inputs[0];
   const c = parseInt(inputs[1]);
 
-  //console.error({b, c}
-  console.error(String.fromCharCode(c) + " -> " + b);
-  // on créer une table qui va permettre à chaque lettre
-  //d'avoir son code
-  coder[String.fromCharCode(c)] = b;
+  console.error(b + " -> " + c);
+  //console.error(b + " -> " + String.fromCharCode(c));
+
+  coder[b] = String.fromCharCode(c);
 }
 console.error(coder);
